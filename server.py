66b8764f05a2631fe13978848286fd0405098d8f
Peter Kugler <kugler.peti@gmail.com>
2019-06-11 15:19:56 +0200

    Implement list, and template changes

--- server.py
+++ server.py
@@ -6,16 +6,14 @@ import data_handler
 app = Flask(__name__)
 
 
-
-@app.route('/')
 @app.route('/list')
 def route_list():
     user_questions = data_handler.get_all_questions()
     return render_template('list.html', user_questions=user_questions)
 
 
-@app.route('/story', methods=['GET', 'POST'])
-def add_story():
+@app.route('/ask-question', methods=['GET', 'POST'])
+def add_question():
     if request.method == 'POST':
         user_story = {
             'title' : request.form.get('title'),
