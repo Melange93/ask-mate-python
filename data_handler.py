46d5b5a7c515b5cbbd6dae8afb6d83b17ca246de
PeterKugler <kugler.peti@gmail.com>
2019-06-12 07:49:53 +0200

    Remove ID from list, Fix data manager typo

--- data_handler.py
+++ data_handler.py
@@ -5,8 +5,8 @@ import util
 
 DATA_FILE_PATH_QUESTIONS = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
 DATA_FILE_PATH_ANSWERS = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
-DATA_HEADER_QUESTIONS  = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-DATA_HEADER_ANSWERS  = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+DATA_HEADER_QUESTIONS = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+DATA_HEADER_ANSWERS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
 
 def get_all_data(convert_linebreaks=False):
