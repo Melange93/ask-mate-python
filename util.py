a08bdc72215017ac92cab26f216ed5ed636cdf5b
PeterKugler <kugler.peti@gmail.com>
2019-06-12 06:53:23 +0200

    uuid key generator

--- util.py
+++ util.py
@@ -1,7 +1,14 @@
 import datetime
+import uuid
 
 
 def from_timestamp_datetime(user_questions):
     for question in user_questions:
         question['submission_time'] = datetime.datetime.fromtimestamp(int(question['submission_time'])).strftime('%Y-%m-%d %H:%M:%S')
     return user_questions
+
+
+def key_generator():
+    key = uuid.uuid4().hex
+    return key
+
