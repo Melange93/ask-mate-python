934d52e6e774f4afb2d73e1140469e7637785f58
PeterKugler <kugler.peti@gmail.com>
2019-06-26 13:06:45 +0200

    Final fix delete question

--- data_handler.py
+++ data_handler.py
@@ -62,11 +62,6 @@ def get_comments(cursor, question_id, answer_id):
 
 @database_common.connection_handler
 def delete_question(cursor, question_id):
-    cursor.execute("""
-                        DELETE FROM answer
-                        WHERE question_id = %s;
-                       """,
-                   (question_id,))
 
     cursor.execute("""
                     DELETE FROM question
