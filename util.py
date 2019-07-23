7439665f9850bf7bb3c7e8118c9942944bee7e64
Tomibomi <bothtamas@hotmail.com>
2019-07-23 14:56:46 +0200

    implement registration function
    known bug:
        - minor bug: registration doesn't check domain on the email field

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import time
 import random
 from flask import request
 import data_handler
+import bcrypt
 
 
 def from_timestamp_datetime(user_questions):
@@ -50,3 +51,13 @@ def add_answer_wrapper(question_id):
         'message': request.form.get('message')
     }
     data_handler.add_new_answer(answer)
+
+def hash_password(plain_text_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
